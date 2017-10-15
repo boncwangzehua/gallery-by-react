27a5b583165e215b13e84166349ab35bb6b1af6c
wangzehua <wangzehua@bonc.com.cn>
2017-10-15 17:04:46 +0800

    start a project

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,30 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('../styles/App.scss');
 
 import React from 'react';
-
-let yeomanImage = require('../images/yeoman.png');
+//获取json中的数据
+var imagesDatas = require('../datas/imagesDatas.json');
+//通过自执行函数根据json中的数据生成images的URL
+imagesDatas = (function genImageURL(imageDataArr){
+    for(var i = 0, j = imageDataArr.length; i < j; i++){
+      var singleImageData = imageDataArr[i];
+      singleImageData.imageURL = require('../images/'+ singleImageData.fileName);
+      imageDataArr[i] = singleImageData[i];
+    }
+    return imageDataArr;
+})(imagesDatas);
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <span>热模块</span>
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <section className="stage">
+        <section className="img-sec">
+
+        </section>
+        <nav className="contorller-nav">
+
+        </nav>
+      </section>
     );
   }
 }
